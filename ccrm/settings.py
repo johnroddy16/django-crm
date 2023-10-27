4950b01708c55d44b74862ec002adb49c7a6c4f3
johnroddy16 <49892127+johnroddy16@users.noreply.github.com>
2023-10-27 12:25:32 -0700

    Update settings.py
    hiding something

--- ccrm/settings.py
+++ ccrm/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "usefulapps",
         "USER": "root",
-        "PASSWORD": "db_yug+61",
+        "PASSWORD": "somepassword",
         "HOST": "localhost",
         "PORT": "3306", 
     }
